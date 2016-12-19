645d897580a24e32c298c9c3d6d9c587c52cbac7
Kieran Trezona-le Comte <kieran@powershop.co.nz>
2016-12-19 18:05:41 +1300

    Fix service spec after changing interface to calculation service

--- spec/services/calculate_mana_base_spec.rb
+++ spec/services/calculate_mana_base_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 RSpec.describe CalculateManaBase do
-  let(:service) { described_class.new(mana_desired, draws) }
+  let(:service) { described_class.new(mana_desired, by_turn) }
 
   context 'with three colours' do
     let(:mana_desired) { { red: 2, blue: 1, white: 1 } }
 
     context 'with 7 draws' do
-      let(:draws) { 7 }
+      let(:by_turn) { 0 }
 
       it 'does a thing' do
         expect(service.call.count).to eq(15)
