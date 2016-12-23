ade8e221077ec672498ba4d2ebe57696c324b827
kylesnowschwartz <kyle.snowschwartz@gmail.com>
2016-12-23 13:31:19 +1300

    add quantifiers to constraints for hand filtering
    this may have the dual benefit of reducing calculation times, as it checks
    less hands, as well as increasing the usefuless by being more specific
    with the quantities of lands you want in a successful hand.

--- spec/controllers/calculations_controller_spec.rb
+++ spec/controllers/calculations_controller_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
 describe CalculationsController, type: :controller do
+  let(:white_constraint) { CalculationsController::ManaConstraint.new(:white, 1, 'at least', 0) }
+  let(:blue_constraint) { CalculationsController::ManaConstraint.new(:blue, 1, 'at least', 0) }
+
+  before do
+    ManaSource.destroy_all
+    ManaSource.create!(white: true, blue: false, black: false, red: false, green: false, colorless: false, name: 'plains', basic: true)
+    ManaSource.create!(white: false, blue: true, black: false, red: false, green: false, colorless: false, name: 'island', basic: true)
+    ManaSource.create!(white: false, blue: false, black: false, red: true, green: false, colorless: false, name: 'mountain', basic: true)
+    ManaSource.create!(white: false, blue: true, black: false, red: true, green: false, colorless: false, name: 'spirebluff_canal', basic: false)
+  end
+
+  after do
+    ManaSource.destroy_all
+  end
+
   describe '#new' do
     it 'renders the new template with 200 OK' do
       get :new
@@ -13,19 +28,14 @@ describe CalculationsController, type: :controller do
   describe '#create' do
     let(:constraints) do
       {
-        constraint_0: { amount: '1', color: 'green', turn: '0' },
-        constraint_1: { amount: '1', color: 'blue', turn: '0' }
+        constraint_0: { amount: '1', quantifier: 'at least', color: 'white', turn: '0' },
+        constraint_1: { amount: '1', quantifier: 'at least', color: 'blue', turn: '0' }
       }
     end
 
-    let(:mana_constraints) do
-      {
-        green: { count: 1, turn: 0 },
-        blue: { count: 1, turn: 0 }
-      }
-    end
+    let(:mana_constraints) { [white_constraint, blue_constraint] }
 
-    let(:lands_to_consider) { ["Spirebluff Canal"] }
+    let(:lands_to_consider) { ["spirebluff canal", "plains", "island"] }
 
     before do
       expect(CalculateManaBase)
@@ -35,7 +45,7 @@ describe CalculationsController, type: :controller do
     end
 
     it 'renders the show template with 200 Ok' do
-      post :create, params: { calculations: constraints, non_basic_lands: ["Spirebluff Canal"] }
+      post :create, params: { calculations: constraints, non_basic_lands: ["spirebluff canal"] }
 
       expect(response).to have_http_status(:ok)
       expect(response).to render_template(:show)
--- spec/services/calculate_mana_base_spec.rb
+++ spec/services/calculate_mana_base_spec.rb
@@ -1,8 +1,14 @@
 require 'spec_helper'
 
 describe CalculateManaBase do
-  let(:service) { described_class.new(mana_desired, lands_to_consider) }
+  let(:service) { described_class.new(mana_constraints, lands_to_consider) }
   let(:result) { service.call }
+  let(:white_constraint) { CalculationsController::ManaConstraint.new(:white, 1, quantifier, 0) }
+  let(:blue_constraint) { CalculationsController::ManaConstraint.new(:blue, 1, quantifier, 0) }
+  let(:red_constraint) { CalculationsController::ManaConstraint.new(:red, 1, quantifier, 0) }
+  let(:mana_constraints) { [white_constraint, blue_constraint] }
+  let(:quantifier) { 'at least' }
+
 
   before do
     ManaSource.destroy_all
@@ -18,15 +24,8 @@ describe CalculateManaBase do
 
   context 'with 1 mana desired for each of 2 colors' do
     let(:lands_to_consider) { ["plains", "island"] }
-    let(:mana_desired) { 
-      {
-        blue: { count: 1, turn: 0 },
-        white: { count: 1, turn: 0 } 
-      } 
-    }
 
     it 'returns the most optimal manabase as an even split of the 2 colors' do
-
       expect(result.first[:island]).to eq(CalculateManaBase::MANA_SOURCES / 2)
       expect(result.first[:plains]).to eq(CalculateManaBase::MANA_SOURCES / 2)
     end
@@ -34,13 +33,7 @@ describe CalculateManaBase do
 
   context 'with 1 mana desired for each of 3 colors' do
     let(:lands_to_consider) { ["plains", "spirebluff_canal", "island", "mountain"] }
-    let(:mana_desired) { 
-      {
-        blue: { count: 1, turn: 0 },
-        white: { count: 1, turn: 0 },
-        red: { count: 1, turn: 0 }
-      } 
-    }
+    let(:mana_constraints) { [white_constraint, blue_constraint, red_constraint] }
 
     it 'returns the most optimal manabase as an even split of the 2 colors' do
       expect(result.first[:spirebluff_canal]).to eq(4)
@@ -51,25 +44,42 @@ describe CalculateManaBase do
 
   context 'when the number of turns is increased' do
     let(:lands_to_consider) { ["plains", "island"] }
-    let(:mana_desired_turn_zero) { 
-      {
-        blue: { count: 1, turn: 0 },
-        white: { count: 1, turn: 0 } 
-      } 
+    let(:mana_constraints_turn_zero) { [white_constraint, blue_constraint]}
+
+    let(:mana_constraints_turn_one) { 
+      [
+        CalculationsController::ManaConstraint.new(:white, 1, quantifier, 0),
+        CalculationsController::ManaConstraint.new(:blue, 1, quantifier, 1)
+      ]
     }
 
-    let(:mana_desired_turn_one) { 
-      {
-        blue: { count: 1, turn: 1 },
-        white: { count: 1, turn: 1 } 
-      } 
-    }
-
-    let(:no_turns) { described_class.new(mana_desired_turn_zero, lands_to_consider) }
-    let(:one_turn) { described_class.new(mana_desired_turn_one, lands_to_consider) }
+    let(:no_turns) { described_class.new(mana_constraints_turn_zero, lands_to_consider) }
+    let(:one_turn) { described_class.new(mana_constraints_turn_one, lands_to_consider) }
 
     it 'returns a higher probability to draw the required mana' do
       expect(no_turns.call.first[:probability]).to be < one_turn.call.first[:probability]
+    end
+  end
+
+  context 'with uneven mana desired for each of 2 colors' do
+    let(:lands_to_consider) { ["plains", "island"] }
+    let(:white_constraint) { CalculationsController::ManaConstraint.new(:white, 3, quantifier, 10) }
+    let(:blue_constraint) { CalculationsController::ManaConstraint.new(:blue, 1, quantifier, 10) }
+    let(:mana_constraints) { [white_constraint, blue_constraint]}
+
+    it 'returns uneven ratio of the 2 colors' do
+      expect(result.first[:island]).to be < result.first[:plains]
+    end
+  end
+
+  context 'with multiple quantifier' do
+    let(:lands_to_consider) { ["plains", "island"] }
+    let(:white_constraint) { CalculationsController::ManaConstraint.new(:white, 3, 'no more than', 10) }
+    let(:blue_constraint) { CalculationsController::ManaConstraint.new(:blue, 1, 'exactly', 10) }
+    let(:mana_constraints) { [white_constraint, blue_constraint]}
+
+    it 'does some cool stuff' do
+      expect(result.first[:island]).to be < result.first[:plains]
     end
   end
 end
